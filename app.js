50904893d836e7e87d148aba590d1d1bfbe182a8
delta <d@qiscus.com>
2014-08-23 19:31:59 +0700

    populated field in the page with data

--- app.js
+++ app.js
@@ -2,33 +2,36 @@
   'use strict';
 
   var app = angular.module('readingList', [])
-    .controller('readingListController', function(){
+    .controller('ReadingListController', function(){
       this.books = books;
+      this.genres = genres;
     });
+
+  var genres = ['non-fiction', 'fiction', 'fantasy']
 
   var books = [
     {
       title: 'Pragmatic Thinking and Learning',
       authors: ['Andy Hunt'],
       isbn: '1934356050',
-      review: 'undefined',
-      rating: 'undefined',
+      review: 'Loved this book!',
+      rating: 4,
       genres: ['non-fiction']
     },
     {
       title: 'The Age of Spiritual Machines',
       authors: ['Ray Kurzweil'],
       isbn: '0140282025',
-      review: 'undefined',
-      rating: 'undefined',
+      review: 'Would not read again',
+      rating: 2,
       genres: ['non-fiction']
     },
     {
       title: 'Elven Star: The Death Gate Cycle',
       authors: ['Margaret Weiss', 'Tracy Hickman'],
       isbn: '0553290983',
-      review: 'undefined',
-      rating: 'undefined',
+      review: 'Best fantasy series EVAR',
+      rating: 5,
       genres: ['fiction', 'fantasy']
     }
   ];
